dc04fc95ead7820929a237037df4c13a656d2da6
David Prakash <118829009+davidprakash12@users.noreply.github.com>
2022-12-27 16:23:56 +0530

    Add files via upload

---
+++ p6iii.py
@@ -0,0 +1,14 @@
+# variable with integer value
+a=12
+# variable with float value
+b=12.56
+# variable with string value
+c="Hello"
+# variable with Boolean value
+d=True
+
+# printing all variables 
+print(a)
+print(b)
+print(c)
+print(d)
